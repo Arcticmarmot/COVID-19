3f87c68a2973f15c850a2e10bd6b07ee9e83ff07
Arcticmarmot <arctic_marmot@outlook.com>
2020-02-26 17:59:49 +0800

    create update-service

---
+++ src/app/services/update.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UpdateService {
+
+  constructor() { }
+}
+//https://view.inews.qq.com/g2/getOnsInfo?name=disease_other&callback=jQuery34103076933412720082_1582704893503&_=1582704893504
+//https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5&callback=jQuery34103076933412720082_1582704893501&_=1582704893502
